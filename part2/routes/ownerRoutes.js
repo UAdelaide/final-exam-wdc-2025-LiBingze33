efe884d055957e5c1636fd4637c5f1da8ef5cc99
LiBingze33 <161436324+LiBingze33@users.noreply.github.com>
2025-06-20 08:08:09 +0000

    [automatic commit] 2025-06-20 08:08:09 UTC

--- part2/routes/ownerRoutes.js
+++ part2/routes/ownerRoutes.js
@@ -2,7 +2,11 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models/db');
 
-
-
+router.get('', (req, res) => {
+  if (!req.session.user) {
+    return res.status(401).json({ error: 'Not logged in' });
+  }
+  res.json(req.session.user);
+});
 
 module.exports = router;
