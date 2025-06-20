616aafaf502a5b1955af3de295b3c664542d08ea
LiBingze33 <161436324+LiBingze33@users.noreply.github.com>
2025-06-20 03:28:06 +0000

    [automatic commit] 2025-06-20 03:28:06 UTC

--- part1/app.js
+++ part1/app.js
@@ -87,7 +87,7 @@ let db;
 
             INSERT INTO WalkRatings (request_id, walker_id, owner_id, rating, comments) VALUES
             (
-            (SELECT request_id FROM WalkRequests WHERE dog_id = (SELECT dog_id FROM Dogs WHERE name = '') AND status = 'completed'),
+            (SELECT request_id FROM WalkRequests WHERE dog_id = (SELECT dog_id FROM Dogs WHERE name = 'Guodong') AND status = 'completed'),
             (SELECT user_id FROM Users WHERE username = 'bobwalker'),
             5,
             'good walker, very reliable'),
