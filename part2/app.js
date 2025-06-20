b3e10dbe2016efa884c8c81bfb44e7a8669d097e
LiBingze33 <161436324+LiBingze33@users.noreply.github.com>
2025-06-20 11:40:29 +0000

    [automatic commit] 2025-06-20 11:40:29 UTC

--- part2/app.js
+++ part2/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const session = require('express-session'); //add the library we need for the session management
 const path = require('path');
-const db = require('../models/db');
+const db = require('./models/db');
 require('dotenv').config();
 
 const app = express();
