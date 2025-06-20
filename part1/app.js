70e5db7988b20f621c5d05e45c6f5729e72db0e5
LiBingze33 <161436324+LiBingze33@users.noreply.github.com>
2025-06-20 02:56:28 +0000

    [automatic commit] 2025-06-20 02:56:28 UTC

--- part1/app.js
+++ part1/app.js
@@ -102,9 +102,9 @@ app.get('/api/dogs',async (req, res) => {
 
 });
 
-app.get('/api/dogs',async (req, res) => {
+app.get('/api/walkrequests/open',async (req, res) => {
     try{
-        const [dogs] = await db.query('SELECT d.name AS dog_name, d.size, u.username AS owner_username FROM Dogs d JOIN Users u ON d.owner_id = u.user_id');
+        const [req] = await db.query('SELECT d.name AS dog_name, d.size, u.username AS owner_username FROM Dogs d JOIN Users u ON d.owner_id = u.user_id');
         res.json(dogs);
     }
     catch(err){
