78eb68193b3b1d63065b3201a6e874f47b781a5d
LiBingze33 <161436324+LiBingze33@users.noreply.github.com>
2025-06-20 03:42:30 +0000

    [automatic commit] 2025-06-20 03:42:30 UTC

--- part1/app.js
+++ part1/app.js
@@ -139,7 +139,7 @@ app.get('/api/walkrequests/open',async (req, res) => {
 
 app.get('/api/walkers/summary',async (req, res) => {
     try{
-        const [requests] = await db.query(SELECT u.username AS walker) );
+        const [requests] = await db.query("SELECT u.username AS walker_username,  ");
         res.json(requests);
     }
     catch(err){
