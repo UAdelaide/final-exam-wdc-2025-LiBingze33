fc40d1d2512d3846df673cd4200fd02cada5de71
LiBingze33 <161436324+LiBingze33@users.noreply.github.com>
2025-06-20 02:25:46 +0000

    [automatic commit] 2025-06-20 02:25:46 UTC

--- part1/app.js
+++ part1/app.js
@@ -52,13 +52,17 @@ let db;
       `);
         }
         const [dog_count] = await db.execute('SELECT COUNT(*) AS count FROM Users');
-        if (user_count[0].count === 0) {
+        if (dog_count[0].count === 0) {
             await db.execute(`
-            INSERT INTO Users (username, email, password_hash, role) VALUES ('alice123', 'alice@example.com', 'hashed123', 'owner'),
-            ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
-            ('carol123', 'carol@example.com', 'hashed789', 'owner'),
-            ('bingze', 'bingze@example.com', 'hashed789', 'owner'),
-            ('bingze123', 'bingze123@example.com', 'hashed789', 'owner');
+            INSERT INTO Dogs (owner_id, name, size)
+
+VALUES
+((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max' , 'medium'),
+((SELECT user_id FROM Users WHERE username = 'carol123'), 'Bella' , 'small'),
+((SELECT user_id FROM Users WHERE username = 'bobwalker'), 'Mike' , 'small'),
+((SELECT user_id FROM Users WHERE username = 'bingze'), 'Toutou' , 'small'),
+((SELECT user_id FROM Users WHERE username = 'bingze123'), 'Guodong' , 'small');
+
 
       `);
         }
