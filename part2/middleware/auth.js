b36324ba509f70d1377dc902f7b98d7341533b50
LiBingze33 <161436324+LiBingze33@users.noreply.github.com>
2025-06-20 14:16:11 +0000

    [automatic commit] 2025-06-20 14:16:11 UTC

--- part2/middleware/auth.js
+++ part2/middleware/auth.js
@@ -6,3 +6,11 @@ function check_owners(req,res,next){
         res.status(403).redirect('/');
     }
 }
+function check_walkers(req,res,next){
+    if(req.session.user && req.session.user.role === 'walker'){
+        next();
+    }
+    else{
+        res.status(403).redirect('/');
+    }
+}
