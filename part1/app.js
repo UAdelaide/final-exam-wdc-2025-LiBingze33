855dd3cbc5a840cb4f6855f468b61d530b234e46
LiBingze33 <161436324+LiBingze33@users.noreply.github.com>
2025-06-20 03:15:21 +0000

    [automatic commit] 2025-06-20 03:15:21 UTC

--- part1/app.js
+++ part1/app.js
@@ -75,6 +75,17 @@ let db;
             ((SELECT dog_id FROM Dogs WHERE name = 'Guodong'), '2025-06-12 13:00:00', 60, 'Qingdao', 'open');
                 `);
         }
+        const [ra] = await db.query('SELECT COUNT(*) AS count FROM WalkRequests');
+        if (request_count[0].count === 0) {
+            await db.execute(`
+            INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status) VALUES
+            ((SELECT dog_id FROM Dogs WHERE name = 'Max'), '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
+            ((SELECT dog_id FROM Dogs WHERE name = 'Bella'), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted'),
+            ((SELECT dog_id FROM Dogs WHERE name = 'Mike'), '2025-06-11 10:00:00', 60, 'Adelaide', 'accepted'),
+            ((SELECT dog_id FROM Dogs WHERE name = 'Toutou'), '2025-06-12 10:00:00', 60, 'China', 'open'),
+            ((SELECT dog_id FROM Dogs WHERE name = 'Guodong'), '2025-06-12 13:00:00', 60, 'Qingdao', 'open');
+                `);
+        }
 
 
     }
