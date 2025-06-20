b1b6bf5631a0770a72af7e3906517349cdf36377
LiBingze33 <161436324+LiBingze33@users.noreply.github.com>
2025-06-20 14:37:19 +0000

    [automatic commit] 2025-06-20 14:37:19 UTC

--- part2/middleware/auth.js
+++ part2/middleware/auth.js
@@ -1,21 +1,21 @@
 //to make the login become more legit here is the middleware implementation
 function check_owners(req,res,next){
-    //check the user is logged in and his role is owner
+    // check the user is logged in and his role is owner
     if(req.session.user && req.session.user.role === 'owner'){
         next();
     }
     else{
-    //if mismatch or not logged in, go back to login page
+    // if mismatch or not logged in, go back to login page
         res.redirect('/');
     }
 }
 function check_walkers(req,res,next){
-    //check the user is logged in and his role is walker
+    // check the user is logged in and his role is walker
     if(req.session.user && req.session.user.role === 'walker'){
         next();
     }
     else{
-    //if mismatched or not logged in, go back to login page
+    // if mismatched or not logged in, go back to login page
         res.redirect('/');
     }
 }
