c92d11e34c31b80e86c4addadb6b8e51ac55c74e
LiBingze33 <161436324+LiBingze33@users.noreply.github.com>
2025-06-20 12:01:00 +0000

    [automatic commit] 2025-06-20 12:01:00 UTC

--- part2/routes/ownerRoutes.js
+++ part2/routes/ownerRoutes.js
@@ -4,10 +4,11 @@ const db = require('../models/db');
 
 router.get('/dogs', async (req, res) => {
     if(!req.session.user){
-        return res.status(401); //will resolve the force browsing later
+        return res.status(401); // will resolve the force browsing later
     }
   try {
-    const OwnerId = req.session.user.user_id; //since user is logged in, just retrieve the id from the session instead of making another database request
+    // since user is logged in, just retrieve the id from the session instead of making another database request
+    const OwnerId = req.session.user.user_id;
     const [dogs] = await db.query('SELECT dog_id, name FROM Dogs where owner_id = ?', [OwnerId]);
     return res.json(dogs);
   }
