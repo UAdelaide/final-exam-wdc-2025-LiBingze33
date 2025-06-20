be7e417413995098eddd1366aee891fad56c11bd
LiBingze33 <161436324+LiBingze33@users.noreply.github.com>
2025-06-20 05:12:58 +0000

    [automatic commit] 2025-06-20 05:12:58 UTC

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -71,8 +71,7 @@ router.post('/login', async (req, res) => {
       user_id:rows[0].user_id,
       username:rows[0].username,
       role:rows[0].role
-
-    }
+    };
     res.json({ message: 'Login successful', user: rows[0] });
   } catch (error) {
     res.status(500).json({ error: 'Login failed' });
