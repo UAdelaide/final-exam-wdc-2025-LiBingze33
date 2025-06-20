1a69a842021cded27d1f0d083221e07e5c204337
LiBingze33 <161436324+LiBingze33@users.noreply.github.com>
2025-06-20 08:08:55 +0000

    [automatic commit] 2025-06-20 08:08:55 UTC

--- part2/routes/ownerRoutes.js
+++ part2/routes/ownerRoutes.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models/db');
 
-router.get('/dogs', async(req, res) = {
-  if (!req.session.user) {
-    return res.status(401).json({ error: 'Not logged in' });
+router.get('/', async (req, res) => {
+  try {
+    const [rows] = await db.query('SELECT user_id, username, email, role FROM Users');
+    res.json(rows);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch users' });
   }
-  res.json(req.session.user);
 });
-
 module.exports = router;
