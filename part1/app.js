7568b11261ce4cbaf30f079dfb26f1e10690bbb9
LiBingze33 <161436324+LiBingze33@users.noreply.github.com>
2025-06-20 03:41:09 +0000

    [automatic commit] 2025-06-20 03:41:09 UTC

--- part1/app.js
+++ part1/app.js
@@ -137,6 +137,17 @@ app.get('/api/walkrequests/open',async (req, res) => {
 
 });
 
+app.get('/api/walkers/summary',async (req, res) => {
+    try{
+        const [requests] = await db.query("SELECT wr.request_id, d.name AS dog_name, wr.requested_time, wr.duration_minutes, wr.location, u.username AS owner_username FROM WalkRequests wr JOIN Dogs d ON wr.dog_id = d.dog_id JOIN Users u ON d.owner_id = u.user_id WHERE wr.status ='open'");
+        res.json(requests);
+    }
+    catch(err){
+        console.log(err);
+        res.status(500).json({error:'cannot fetch requests'});
+    }
+
+});
 
 
 app.use(logger('dev'));
