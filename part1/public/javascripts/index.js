dcca73d3ed5fb6e8a136c9a6e744a96b32e021a9
LiBingze33 <161436324+LiBingze33@users.noreply.github.com>
2025-06-20 04:28:15 +0000

    [automatic commit] 2025-06-20 04:28:15 UTC

--- part1/public/javascripts/index.js
+++ part1/public/javascripts/index.js
@@ -8,6 +8,7 @@ const vueinst = Vue.createApp({
         try{
             const response = await fetch('https://dog.ceo/api/breeds/image/random');
             const data = await response.json();
+            console.log(data.message);
             this.dog_img_url = data.message;
             }
         catch(err){
