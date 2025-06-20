aab302ec87e7347b8eeb4b4ea6cf93ebaf7211d1
LiBingze33 <161436324+LiBingze33@users.noreply.github.com>
2025-06-20 14:30:00 +0000

    [automatic commit] 2025-06-20 14:30:00 UTC

--- part2/app.js
+++ part2/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const session = require('express-session'); //add the library we need for the session management
 const path = require('path');
 const db = require('./models/db');
-const{check_owners, check_walkers} = require("./middleware/auth")
+const{check_owners, check_walkers} = require("./middleware/auth");
 require('dotenv').config();
 
 const app = express();
