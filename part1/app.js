0fa922f48515d48af48c413230ecef3b83ee98ad
LiBingze33 <161436324+LiBingze33@users.noreply.github.com>
2025-06-21 04:51:19 +0000

    [automatic commit] 2025-06-21 04:51:19 UTC

--- part1/app.js
+++ part1/app.js
@@ -78,7 +78,7 @@ let db;
             );
         `);
         await db.execute(`
-            CREATE TABLE WalkRatings (
+            CREATE TABLE IF NOT EXIST WalkRatings (
                 rating_id INT AUTO_INCREMENT PRIMARY KEY,
                 request_id INT NOT NULL,
                 walker_id INT NOT NULL,
