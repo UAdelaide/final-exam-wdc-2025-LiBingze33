29d5e95590691b59f960920d25abc4b78bf73011
LiBingze33 <161436324+LiBingze33@users.noreply.github.com>
2025-06-21 05:09:12 +0000

    [automatic commit] 2025-06-21 05:09:12 UTC

--- part1/app.js
+++ part1/app.js
@@ -211,11 +211,11 @@ app.get('/api/walkers/summary', async (req, res) => {
 });
 
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(logger('dev'));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
