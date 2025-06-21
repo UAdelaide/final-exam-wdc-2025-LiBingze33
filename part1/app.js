755912f03881683dbd25c9c9d06f8e337d336ed0
LiBingze33 <161436324+LiBingze33@users.noreply.github.com>
2025-06-21 04:57:38 +0000

    [automatic commit] 2025-06-21 04:57:38 UTC

--- part1/app.js
+++ part1/app.js
@@ -125,7 +125,7 @@ let db;
             ((SELECT dog_id FROM Dogs WHERE name = 'Max'), '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
             ((SELECT dog_id FROM Dogs WHERE name = 'Bella'), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted'),
             ((SELECT dog_id FROM Dogs WHERE name = 'Mike'), '2025-06-11 10:00:00', 60, 'Adelaide', 'accepted'),
-            ((SELECT dog_id FROM Dogs WHERE name = 'Toutou'), '2025-06-12 10:00:00', 60, 'China', 'completed'),
+            ((SELECT dog_id FROM Dogs WHERE name = 'Toutou'), '2025-06-12 10:00:00', 60, 'China', 'open'),
             ((SELECT dog_id FROM Dogs WHERE name = 'Guodong'), '2025-06-12 13:00:00', 60, 'Qingdao', 'completed');
                 `);
         }
